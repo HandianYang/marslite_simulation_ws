07bffa58014c770d166209bdbeaa7317cddf7779
HandianYang <ych0610765@gmail.com>
2024-11-18 19:28:08 +0800

    v5.0.1-Upgrade Docker

--- docker_run.sh
+++ docker_run.sh
@@ -2,8 +2,8 @@
 xhost +local:docker
 
 # Setup specific docker image and tag
-DOCKER_IMAGE="handianyang/marslite_simulation"
-DOCKER_LATEST_TAG="v1.2.0-T265"
+DOCKER_IMAGE="handianyang/marslite-simulation"
+DOCKER_DEFAULT_TAG="cuda12.1.1-pytorch2.5.1-nvidia535-noetic"
 CONTAINER_DEFAULT_NAME="marslite"
 
 # Setup the style of color
@@ -11,9 +11,7 @@ COLOR_RED='\033[0;31m'
 COLOR_YELLOW='\033[0;33m'
 COLOR_NC='\033[0m'
 
-#
 # Check the command 'nvidia-docker' existed or not
-#
 ret_code="$(command -v nvidia-docker)"
 if [ -z "$ret_code" ]; then
     if [ -z "$(command -v nvidia-container-cli)" ]; then
@@ -27,70 +25,46 @@ else
     NVIDIA_SUPPORT_OPTION=""
 fi
 
-#
-# Specify docker image tag
-#
-run_docker=true
-if [ $# -gt 0 ]; then
-    if [ "$1" == "base" ] || [ "$1" == "100" ]; then
-	    DOCKER_TAG="v1.0.0-Base"
-    elif [ "$1" == "110" ]; then
-    	DOCKER_TAG="v1.1.0-MPC"
-    elif [ "$1" == "111" ]; then
-    	DOCKER_TAG="v1.1.1-MPC"
-    elif [ "$1" == "112" ]; then
-    	DOCKER_TAG="v1.1.2-MPC-Bugfix"
-    fi
-    CONTAINER_NAME="${CONTAINER_DEFAULT_NAME}_prev"
-else
-    DOCKER_TAG=$DOCKER_LATEST_TAG
-    CONTAINER_NAME=$CONTAINER_DEFAULT_NAME
-fi
 
+# Find current directory and transfer it to container directory for Docker
+current_dir="$(pwd)"
+host_dir="${HOME}/"
+container_dir="/home/developer/"
+goal_dir=${current_dir//$host_dir/$container_dir}
 
-# Check if we need to run docker
-if $run_docker; then
-    echo "[IMAGE:TAG] $DOCKER_IMAGE:$DOCKER_TAG"
+echo "[IMAGE:TAG] $DOCKER_IMAGE:$DOCKER_DEFAULT_TAG"
 
-    # Find current directory and transfer it to container directory for Docker
-    current_dir="$(pwd)"
-    host_dir="${HOME}/"
-    container_dir="/home/developer/"
-    goal_dir=${current_dir//$host_dir/$container_dir}
-
-    # Check if the Docker container exists
-    if [ "$(docker ps -a -q -f name=$CONTAINER_NAME)" ]; then
-        # The container exists, check if it's running or stopped
-        if [ "$(docker inspect -f '{{.State.Running}}' $CONTAINER_NAME)" == "true" ]; then
-            echo "The \"$CONTAINER_NAME\" container is RUNNING, so enter it."
-            docker exec -it ${CONTAINER_NAME} bash
-        else
-            echo "The \"$CONTAINER_NAME\" container is STOPPED, so restart it."
-            docker start -ai ${CONTAINER_NAME}
-        fi
+# Check if the Docker container exists
+if [ "$(docker ps -a -q -f name=$CONTAINER_NAME)" ]; then
+    # The container exists, check if it's running or stopped
+    if [ "$(docker inspect -f '{{.State.Running}}' $CONTAINER_NAME)" == "true" ]; then
+        echo "The \"$CONTAINER_NAME\" container is RUNNING, so enter it."
+        docker exec -it ${CONTAINER_NAME} bash
     else
-        echo "The \"$CONTAINER_NAME\" container DOES NOT EXIST, so create a new container."
-        ${DOCKER_CMD} run -it --privileged ${NVIDIA_SUPPORT_OPTION} \
-            --name ${CONTAINER_NAME} \
-            --net=host \
-            --env DISPLAY=$DISPLAY \
-            --env QT_X11_NO_MITSHM=1 \
-            -v /dev:/dev \
-            -v /etc/localtime:/etc/localtime:ro \
-            -v /var/run/docker.sock:/var/run/docker.sock \
-            -v /tmp/.X11-unix/:/tmp/.X11-unix:rw \
-            -v /dev/bus/usb:/dev/bus/usb \
-            -v ${current_dir}:${goal_dir} \
-            -w ${goal_dir} \
-            --device=/dev/snd \
-            --device=/dev/dri \
-            --device=/dev/nvhost-ctrl \
-            --device=/dev/nvhost-ctrl-gpu \
-            --device=/dev/nvhost-prof-gpu \
-            --device=/dev/nvmap \
-            --device=/dev/nvhost-gpu \
-            --device=/dev/nvhost-as-gpu \
-            ${DOCKER_IMAGE}:${DOCKER_TAG}  
+        echo "The \"$CONTAINER_NAME\" container is STOPPED, so restart it."
+        docker start -ai ${CONTAINER_NAME}
     fi
+else
+    echo "The \"$CONTAINER_NAME\" container DOES NOT EXIST, so create a new container."
+    ${DOCKER_CMD} run -it --privileged ${NVIDIA_SUPPORT_OPTION} \
+        --name ${CONTAINER_NAME} \
+        --net=host \
+        --env DISPLAY=$DISPLAY \
+        --env QT_X11_NO_MITSHM=1 \
+        -v /dev:/dev \
+        -v /etc/localtime:/etc/localtime:ro \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -v /tmp/.X11-unix/:/tmp/.X11-unix:rw \
+        -v /dev/bus/usb:/dev/bus/usb \
+        -v ${current_dir}:${goal_dir} \
+        -w ${goal_dir} \
+        --device=/dev/snd \
+        --device=/dev/dri \
+        --device=/dev/nvhost-ctrl \
+        --device=/dev/nvhost-ctrl-gpu \
+        --device=/dev/nvhost-prof-gpu \
+        --device=/dev/nvmap \
+        --device=/dev/nvhost-gpu \
+        --device=/dev/nvhost-as-gpu \
+        ${DOCKER_IMAGE}:${DOCKER_DEFAULT_TAG}  
 fi
-
